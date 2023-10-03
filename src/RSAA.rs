156ffd7c2e9b96e34e6f6f1e59378a23d4b61101
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2023-10-03 18:12:50 +0200

    minor changes

--- src/RSAA.rs
+++ src/RSAA.rs
@@ -1,12 +1,14 @@
+use std::result;
+
 use crate::primeGenerator::PrimeGenerator;
 
 
 
 struct PrivateKey {
-  P: i64,
-  Q: i64,
-  d: i64,
-  e: i64
+  pub P: i64,
+  pub Q: i64,
+  pub d: i64,
+  pub e: i64
 }
 
 impl PrivateKey {
@@ -16,6 +18,9 @@ impl PrivateKey {
       PrivateKey { P: p, Q: q,e : 65537, d: 0 }
     }
 
+    fn get_N(&self) -> i64 {
+      self.P * self.Q
+    }
 
     fn euclid(a: i64,b: i64) -> i64 {
       let r = a%b;
@@ -39,15 +44,37 @@ impl PublicKey {
 }
 
 struct RSAA {
-  privateKey: Option<PrivateKey>
+  privateKey: Option<PrivateKey>,
+  publicKey: Option<PublicKey>
 
 } 
 
 impl RSAA {
   pub fn new() -> Self {
-    RSAA { privateKey: None }
+    RSAA { privateKey: None, publicKey: None }
   }
 
+  fn encrypt(&self, message: String) -> Result<Vec<i64>, &str> {
+    if self.publicKey.is_none() { return Err("no public key")}
+    let mut result = Vec::new();
+
+
+    return Ok(result);
+  }
+
+  fn decrypt(&self, encrypted: Vec<i64>) -> Result<String, &str> {
+    let private_key = match &self.privateKey {
+      Some(key) => key,
+      None => return Err("No hay clave privada"),
+  };
+
+    let mut result = "".to_string();
+    for i in encrypted.iter() {
+      let decrypted = i.pow(private_key.d.try_into().unwrap()) % private_key.get_N();
+      result.push_str(&decrypted.to_string());
+    }
+    return Ok(result);
+  }
   
 
 }
